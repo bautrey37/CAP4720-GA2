0dfe55a066abec7d6628bb4214f9be671c97f014
Colegreen <ColeGarner@knights.ucf.edu>
2013-11-13 17:12:25 -0500

    Added simultaneous Key Support

--- src/GA2.js
+++ src/GA2.js
@@ -38,57 +38,99 @@
             var angle = 0;
             var modelList = document.getElementById("modelList");
             loadModel(modelList.options[modelList.selectedIndex].value);
-			var interval = null;
+			var intervalL = null, intervalR = null, intervalU = null, intervalDo = null;
+			var intervalW = null, intervalA = null, intervalS = null, intervalD = null;
 	
 			// Set up key listeners to move through the world.
 			window.onkeydown = 
 			function(e) {
-				if (interval == null) {
-					// Create new interval 
-					interval = setInterval(function() {
-						if (e.keyCode == 37) {
+				if(e.keyCode == 37 && intervalL == null) {
+					intervalL = setInterval(function() {
 							// User pressed left arrow.  Pan (Look left)
 							viewMatrix = camera.panLeft();
-						}
-						if (e.keyCode == 39) {
-							// User pressed right arrow. Pan (Look right)
+					}, 100);
+				}
+				if(e.keyCode == 39 && intervalR == null) {
+					intervalR = setInterval(function() {
+							// User pressed right arrow.  Pan (Look right)
 							viewMatrix = camera.panRight();
-						}
-						if (e.keyCode == 38) {
-							// User pressed up arrow. Tilt (Look up)
+					}, 100);
+				}
+				if(e.keyCode == 38 && intervalU == null) {
+					intervalU = setInterval(function() {
+							// User pressed up arrow.  Tilt (Look up)
 							viewMatrix = camera.tiltUp();
-						}	
-						if (e.keyCode == 40) {
-							// User pressed down arrow. Tilt (Look down)
+					}, 100);
+				}
+				if(e.keyCode == 40 && intervalDo == null) {
+					intervalDo = setInterval(function() {
+							// User pressed down arrow.  Tilt (Look down)
 							viewMatrix = camera.tiltDown();
-						}
-						if (e.keyCode == 65) {
+					}, 100);
+				}
+				if(e.keyCode == 65 && intervalA == null) {
+					intervalA = setInterval(function() {
 							// User pressed 'a' key. Truck (Step left)
 							viewMatrix = camera.truckLeft();
 							// The light source is always at the eye.
 							model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
-						}
-						if (e.keyCode == 68) {
+					}, 100);
+				}
+				if(e.keyCode == 68 && intervalD == null) {
+					intervalD = setInterval(function() {
 							// User pressed 'd' key. Truck (Step right)
 							viewMatrix = camera.truckRight();
 							// The light source is always at the eye.
 							model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
-						}
-						if (e.keyCode == 87) {
+					}, 100);
+				}
+				if(e.keyCode == 87 && intervalW == null) {
+					intervalW = setInterval(function() {
 							// User pressed 'w' key. Dolly (Step in)
 							viewMatrix = camera.dollyToward();
-						}
-						if (e.keyCode == 83) {
+					}, 100);
+				}
+				if(e.keyCode == 83 && intervalS == null) {
+					intervalS = setInterval(function() {
 							// User pressed 's' key. Dolly (Step back)
 							viewMatrix = camera.dollyBack();
-						}
 					}, 100);
 				}
 			}
 			// Clear previous keydown interval.
-			window.onkeyup = function() {
-				clearInterval(interval);
-				interval = null;
+			window.onkeyup = function(e) {
+				if(e.keyCode == 37) {
+					clearInterval(intervalL);
+					intervalL = null;
+				}
+				if(e.keyCode == 39) {
+					clearInterval(intervalR);
+				intervalR = null;
+				}
+				if(e.keyCode == 38) {
+					clearInterval(intervalU);
+					intervalU = null;
+				}
+				if(e.keyCode == 40) {
+					clearInterval(intervalDo);
+					intervalDo = null;
+				}
+				if(e.keyCode == 65) {
+					clearInterval(intervalA);
+					intervalA = null;
+				}
+				if(e.keyCode == 68) {
+					clearInterval(intervalD);
+					intervalD = null;
+				}
+				if(e.keyCode == 87) {
+					clearInterval(intervalW);
+					intervalW = null;
+				}
+				if(e.keyCode == 83) {
+					clearInterval(intervalS);
+					intervalS = null;
+				}
 			}
 			
             function draw() {
